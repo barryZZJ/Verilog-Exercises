11ca26ddd3228028bd05a69b67aeff3070d7c253
Bazinga <850462618@qq.com>
2019-10-28 21:06:31 +0800

    数码管显示模块

--- school_project/scources/6_adder_subtractor/decoder2to4.v
+++ school_project/scources/6_adder_subtractor/decoder2to4.v
@@ -3,10 +3,10 @@ module decoder2to4(input[1:0] x,
                    output reg[3:0] y);
     always @(x) begin
         case (x)
-            2'b00: y = 4'b1110;
-            2'b01: y = 4'b1101;
-            2'b10: y = 4'b1011;
-            2'b11: y = 4'b0111;
+            0: y = 4'b1110;
+            1: y = 4'b1101;
+            2: y = 4'b1011;
+            3: y = 4'b0111;
             default: y = 4'b0;
         endcase
     end
--- sources/display_num/display_16bto4h.v
+++ sources/display_num/display_16bto4h.v
@@ -1,18 +1,17 @@
 //数码管显示模块
 //能将16位的二进制数以十六进制形式显示到4个七段数码管上
 //负数则小数点代表负号
-//BASYS3板子自带的时钟CLK频率是100MHz，10^5即为 1ms
+//BASYS3板子自带的时钟CLK频率是100MHz，即每秒tick 10^8次。所以10^5即为 1ms
 module display_16bto4h #(parameter INTERVAL = 10**5/2)//控制数码管频率，为1/INTERVAL Hz
-                        (input CLK,
+                        (input CLK,//板子自带的clock
                          input [15:0] x, //要显示的16位数
-                         input [3:0] neg, //各位是否为负数
-                         output reg[11:0] DISP);
+                         input [3:0] neg, //negative，控制各位是否为负数
+                         output reg[11:0] DISP); // DISPLAY，数码管
 
-    integer counter = 0;
-    reg CLK_2 = 0;
+    integer counter = 0;//计数器，记CLK tick的次数
 
-    reg[1:0] sel = 0; //选择显示哪一位
-    wire[3:0] posb; //独热码，对应DISP前4位数，由sel控制，低电平有效
+    reg[1:0] sel = 0; //select，选择显示哪一位，取十进制数0~3，最后转换成posb四位独热码
+    wire[3:0] posb; //pos binary，四位独热码，对应DISP前4位控制位，由sel控制，低电平有效，作用为：“DISP <= {posb, btohDISP(digit), dp};”
     reg[3:0] digit; //取输入端口x的某4位
     reg dp; //小数点
 
@@ -21,20 +20,14 @@ module display_16bto4h #(parameter INTERVAL = 10**5/2)//控制数码管频率，
     always @(posedge CLK) begin
         counter <= counter + 1;
         if (counter == INTERVAL)begin
-            //CLK每tick INTERVAL次CLK_2就tick一次
-            CLK_2 = ~CLK_2;
-            counter <= 0;
+            //sel依次取0, 1, 2, 3, 0, 1, 2, 3...每次控制一个数码管
+            sel <= sel + 1;
+            if (sel == 3) sel <= 0;
+            counter <= 0; //重置counter
         end
     end
 
-    always @(posedge CLK_2) begin
-        //CLK_2每tick一次数码管就显示下一位
-        sel <= sel + 1;
-        if (sel == 3)
-            sel <= 0;
-    end
-
-    decoder2to4 u_dec(sel, posb);//decode sel into posb，低电平有效
+    decoder2to4 u_dec(sel, posb);//低电平有效, 把0, 1, 2, 3对应转换成1110, 1101, 1011, 0111
 
     always @(sel) begin
         //digit对应取x的某4位
@@ -46,7 +39,7 @@ module display_16bto4h #(parameter INTERVAL = 10**5/2)//控制数码管频率，
         dp = ~neg[sel];
     end
 
-    //display 4'bin into hex display
+    //控制显示数字的7~1位
     function [6:0] btohDISP; 
         input[3:0] digit;
         case (digit)
@@ -72,6 +65,7 @@ module display_16bto4h #(parameter INTERVAL = 10**5/2)//控制数码管频率，
     endfunction
 
     always @(posedge CLK_2) begin
+    //把各个位组合起来显示在数码管上
         DISP <= {posb, btohDISP(digit), dp};
     end
 
